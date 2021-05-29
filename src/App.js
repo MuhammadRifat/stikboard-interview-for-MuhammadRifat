c97917c6e8048fd6fe64510236f1e8ff1a452010
Muhammad Rifat <hrifat450@gmail.com>
2021-05-29 11:32:56 +0600

    PrivateRoute and Header done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import {
   Link
 } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 export const userContext = createContext();
 
@@ -16,13 +18,14 @@ function App() {
   return (
     <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
+        <Header></Header>
         <Switch>
           <Route path="/login">
             <Login></Login>
           </Route>
-          <Route exact path="/">
+          <PrivateRoute exact path="/">
             <Dashboard></Dashboard>
-          </Route>
+          </PrivateRoute>
           <Route path="*">
             <NotFound></NotFound>
           </Route>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { Container, Image } from 'react-bootstrap';
+import { userContext } from '../../App';
+import logo from '../../images/spacex-logo1.png';
+
+const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
+    return (
+        <Container className="text-center">
+            <Image className="w-50" src={logo} alt="" />
+            {
+                loggedInUser.email &&
+                    <button className="bg-light" style={{ border: 'none', borderRadius: '5px', marginLeft: '20px' }}>Log out</button>
+            }
+        </Container>
+    );
+};
+
+export default Header;
